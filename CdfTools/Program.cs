f94df3c692b8c6f6e4caa7371972662868785730
John Dziurlaj <john@hiltonroscoe.com>
2018-08-19 17:05:58 -0400

    seperate out child class

--- CdfTools/Program.cs
+++ CdfTools/Program.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Drawing;
-using System.IO;
-using System.Xml;
-using System.Xml.Linq;
-using System.Xml.Schema;
-using System.Xml.Xsl;
-using McMaster.Extensions.CommandLineUtils;
+﻿using McMaster.Extensions.CommandLineUtils;
 namespace CdfValidator
 {
-    [HelpOption("--my-help")]
+    [HelpOption("--hlp")]
     [Command(ExtendedHelpText = "Tools for working with Common Data Formats",
     Name = "cdftools",
     FullName = "Common Data Format Tools")]
-
     [Subcommand("validate", typeof(ValidateCommand))]
     class Program
     {
@@ -24,47 +15,5 @@ namespace CdfValidator
             app.ShowHelp();
             return 1;
         }
-
-    }
-    [HelpOption("--my-help")]
-    [Command(ExtendedHelpText = "Validates Common Data Format instances.")]
-    class ValidateCommand
-    {
-
-        [LegalFilePath]
-        [Option(Template = "-i|--input", Description = "Input instance file")]
-        [Required]
-        public string Input { get; }
-
-        [LegalFilePath]
-        [Option(Template = "-s|--schema", Description = "Input schema file")]
-        [Required]
-        public string Schema { get; }
-
-        [Option(Template = "-v|--verbose")]
-        public bool Verbose { get; }
-
-        public void OnExecute()
-        {
-            var validator = new XsdValidator();
-            var errorList = validator.Validate(this.Schema, this.Input);
-            if (errorList.Count > 0)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                foreach (var error in errorList)
-                {
-                    Console.WriteLine(error);
-                }
-                Console.WriteLine("XML Instance has one or more errors");
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("XML Instance is Valid");
-            }
-            Console.ResetColor();
-        }
-
-        public bool HasErrors { get; set; }
     }
 }
