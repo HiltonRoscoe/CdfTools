d27060a9e10b23bf0724f14e77d75a14503090dc
John Dziurlaj <john@hiltonroscoe.com>
2019-05-04 13:08:39 -0400

    more fixes

---
+++ CdfTools.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using CdfTools;
+namespace CdfTools.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+           /* var sv = new CdfTools.SchematronValidator();
+            var result = CdfTools.SchematronValidator.Schematron();
+            var nodes = result.SelectNodes("/svrl:schematron-output/svrl:failed-assert");
+            foreach(var node in nodes){
+             //   nodes.
+            }
+            Console.Write(result.ToString());
+            // Assert.False(true);
+            */
+        }
+    }
+}
